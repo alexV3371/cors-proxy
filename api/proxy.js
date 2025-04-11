2b873f679186bc4d9a486266573161b7b0eefb26
alexV3371 <alexeyv453@gmail.com>
2025-04-11 20:00:27 +0300

    proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const targetURL = "https://script.google.com/macros/s/AKfycbwGV0Lo1oTahWSKOvKPdMKAQS0czQJdre1dxiq_ucUFshu1IMa82sPUre_Nqy8TCRQy9w/exec; // ← вставь свой ID
+  const targetURL = "https://script.google.com/macros/s/AKfycbwGV0Lo1oTahWSKOvKPdMKAQS0czQJdre1dxiq_ucUFshu1IMa82sPUre_Nqy8TCRQy9w/exec";
 
   try {
     const googleRes = await fetch(targetURL, {
@@ -15,9 +15,10 @@ export default async function handler(req, res) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
     res.status(200).send(result);
   } catch (err) {
-    console.error(err);
+    console.error("❌ Ошибка в прокси:", err);
     res.status(500).send("Ошибка прокси");
   }
 }
